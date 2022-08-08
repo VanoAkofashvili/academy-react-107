d50632094f98c210de453410ee0a34e8ac6d051e
Vaniko Akopashvili <v.akopashvili@sweeftdigital.com>
2022-08-08 18:35:04 +0400

    message

--- 2/lecture/what_is_react/using_react/src/App.js
+++ 2/lecture/what_is_react/using_react/src/App.js
@@ -1,4 +1,4 @@
-import Card from './components/Card';
+import Card from "./components/Card";
 
 function App() {
   return (
@@ -7,10 +7,10 @@ function App() {
         title="Learn React"
         image="https://miro.medium.com/max/500/1*cPh7ujRIfcHAy4kW2ADGOw.png"
       />
-      {/* <Card
-        title="Learn Node"
-        image="https://iconape.com/wp-content/png_logo_vector/node-js-2.png"
-      /> */}
+      {/*<Card*/}
+      {/*  title="Learn Node"*/}
+      {/*  image="https://iconape.com/wp-content/png_logo_vector/node-js-2.png"*/}
+      {/*/>*/}
     </section>
   );
 }
